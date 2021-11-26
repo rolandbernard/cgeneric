07efc7d024ccbd0dfe73e8a71df4bf04efe92bd5
Roland Bernard <rolli.bernard@gmail.com>
2021-11-27 00:03:23 +0100

    Implemented b-tree delete

--- src/b-tree/b-tree.c
+++ src/b-tree/b-tree.c
@@ -69,7 +69,7 @@ static size_t TYPED(lowerBound)(TYPE* array, size_t size, TYPE value) {
     return i;
 #else
     size_t i = 0;
-    while (LESS_THAN(array[i], value)) {
+    while (i < size && LESS_THAN(array[i], value)) {
         i++;
     }
     return i;
@@ -114,18 +114,10 @@ static void TYPED(insertIntoBTreeNode)(
             right->child[j] = node->child[node->count / 2 + j];
         }
         right->child[right->count] = node->child[node->count];
-        if (right->child[0] != NULL) {
-            for (size_t j = 0; j <= right->count; j++) {
-                right->child[j]->parent = right;
-            }
-        }
         TYPE median;
         if (at == node->count / 2) {
             median = value;
-            right->child[right->count] = child;
-            if (child != NULL) {
-                child->parent = right;
-            }
+            right->child[0] = child;
         } else if (at < node->count / 2) {
             median = node->values[node->count / 2 - 1];
             for (size_t j = node->count / 2 - 1; j > at;) {
@@ -140,14 +132,17 @@ static void TYPED(insertIntoBTreeNode)(
             }
         } else {
             median = right->values[0];
-            for (size_t j = 0; j < at - node->count / 2; j++) {
+            for (size_t j = 0; j < at - 1 - node->count / 2; j++) {
                 right->values[j] = right->values[j + 1];
-                right->child[j + 1] = right->child[j + 2];
+                right->child[j] = right->child[j + 1];
             }
-            right->values[at - node->count / 2] = value;
-            right->child[at - node->count / 2 + 1] = child;
-            if (child != NULL) {
-                child->parent = right;
+            right->child[at - 1 - node->count / 2] = right->child[at - node->count / 2];
+            right->values[at - 1 - node->count / 2] = value;
+            right->child[at - node->count / 2] = child;
+        }
+        if (right->child[0] != NULL) {
+            for (size_t j = 0; j <= right->count; j++) {
+                right->child[j]->parent = right;
             }
         }
         node->count /= 2;
@@ -205,7 +200,121 @@ void TYPED(insertIntoBTree)(TYPED(BTree)* tree, TYPE element) {
     }
 }
 
-void TYPED(deleteFromBTree)(TYPED(BTree)* tree, TYPE element);
+static void TYPED(deleteRebalanceBTreeNode)(TYPED(BTree)* tree, TYPED(BTreeNode)* node) {
+    if (node->count == 0 && node->parent == NULL) {
+        tree->root = node->child[0];
+        FREE(node);
+    } else if (node->count < CAPACITY / 2 && node->parent != NULL) {
+        size_t d = TYPED(positionOfChild)(node->parent, node);
+        if (
+            d > 0 && node->parent->child[d - 1] != NULL
+            && node->parent->child[d - 1]->count > CAPACITY / 2
+        ) {
+            TYPED(BTreeNode)* sib = node->parent->child[d - 1];
+            node->count++;
+            for (size_t i = node->count; i > 1;) {
+                i--;
+                node->values[i] = node->values[i - 1];
+                node->child[i + 1] = node->child[i];
+            }
+            node->child[1] = node->child[0];
+            node->values[0] = node->parent->values[d - 1];
+            node->child[0] = sib->child[sib->count];
+            if (node->child[0] != NULL) {
+                node->child[0]->parent = node;
+            }
+            node->parent->values[d - 1] = sib->values[sib->count - 1];
+            sib->count--;
+        } else if (
+            d < node->parent->count && node->parent->child[d + 1] != NULL
+            && node->parent->child[d + 1]->count > CAPACITY / 2
+        ) {
+            TYPED(BTreeNode)* sib = node->parent->child[d + 1];
+            node->values[node->count] = node->parent->values[d];
+            node->child[node->count + 1] = sib->child[0];
+            if (node->child[node->count + 1] != NULL) {
+                node->child[node->count + 1]->parent = node;
+            }
+            node->parent->values[d] = sib->values[0];
+            node->count++;
+            for (size_t i = 1; i < sib->count; i++) {
+                sib->values[i - 1] = sib->values[i];
+                sib->child[i - 1] = sib->child[i];
+            }
+            sib->child[sib->count - 1] = sib->child[sib->count];
+            sib->count--;
+        } else {
+            TYPED(BTreeNode) * left;
+            TYPED(BTreeNode) * right;
+            if (d > 0) {
+                left = node->parent->child[d - 1];
+                right = node;
+                d--;
+            } else {
+                left = node;
+                right = node->parent->child[d + 1];
+            }
+            left->values[left->count] = node->parent->values[d];
+            left->child[left->count + 1] = right->child[0];
+            if (left->child[left->count + 1] != NULL) {
+                left->child[left->count + 1]->parent = left;
+            }
+            left->count++;
+            for (size_t i = 0; i < right->count; i++) {
+                left->values[left->count] = right->values[i];
+                left->child[left->count + 1] = right->child[i + 1];
+                if (left->child[left->count + 1] != NULL) {
+                    left->child[left->count + 1]->parent = left;
+                }
+                left->count++;
+            }
+            FREE(right);
+            for (size_t i = d + 1; i < left->parent->count; i++) {
+                left->parent->values[i - 1] = left->parent->values[i];
+                left->parent->child[i] = left->parent->child[i + 1];
+                if (left->parent->child[i] != NULL) {
+                    left->parent->child[i]->parent = left->parent;
+                }
+            }
+            left->parent->count--;
+            TYPED(deleteRebalanceBTreeNode)(tree, left->parent);
+        }
+    }
+}
+
+static void TYPED(deleteFromBTreeNode)(TYPED(BTree)* tree, TYPED(BTreeNode)* node, size_t at) {
+    if (node->child[at + 1] != NULL) {
+        TYPED(BTreeNode)* c = node->child[at + 1];
+        while (c->child[0] != NULL) {
+            c = c->child[0];
+        }
+        node->values[at] = c->values[0];
+        TYPED(deleteFromBTreeNode)(tree, c, 0);
+    } else {
+        for (size_t i = at + 1; i < node->count; i++) {
+            node->values[i - 1] = node->values[i];
+        }
+        node->count--;
+        TYPED(deleteRebalanceBTreeNode)(tree, node);
+    }
+}
+
+void TYPED(deleteFromBTree)(TYPED(BTree)* tree, TYPE element) {
+    if (tree->root != NULL) {
+        TYPED(BTreeNode)* c = tree->root;
+        while (c != NULL) {
+            size_t d = TYPED(lowerBound)(c->values, c->count, element);
+            if (
+                d < c->count && !LESS_THAN(c->values[d], element)
+                && !LESS_THAN(element, c->values[d])
+            ) {
+                TYPED(deleteFromBTreeNode)(tree, c, d);
+                return;
+            }
+            c = c->child[d];
+        }
+    }
+}
 
 TYPED(BTreeIterator) TYPED(getBTreeIterator)(TYPED(BTree)* tree) {
     TYPED(BTreeNode)* c = tree->root;
